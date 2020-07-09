17db2ddeb21715d10aec340983f09c87066f08bf
Filipa22 <65426259+Filipa22@users.noreply.github.com>
2020-07-09 19:31:28 +0100

    Added convertion, wind speed, humidity and weather description

--- src/index.js
+++ src/index.js
@@ -5,19 +5,29 @@ function showWeather(response) {
   let tempMax = document.querySelector("#temp-max");
   let tempMin = document.querySelector("#temp-min");
   let iconElement = document.querySelector("#main-icon");
+  let weathdescrip = document.querySelector("#weathdescrip");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
   let searchLat = `${response.data.coord.lat}`;
   let searchLon = `${response.data.coord.lon}`;
+  celsiusTemperature = response.data.main.temp;
+  cityfer = response.data.name;
+  citycel = response.data.name;
   city.innerHTML = `${response.data.name}`;
   tempe.innerHTML = `${temperature}`;
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  weathdescrip.innerHTML = `${response.data.weather[0].description} |`;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}% |`;
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+
   tempMax.innerHTML = `${Math.round(response.data.main.temp_max)}ºC`;
   tempMin.innerHTML = `${Math.round(response.data.main.temp_min)}ºC`;
 
   function showGeo(response) {
-    let forecastElement = document.querySelector("#teste-week");
+    let forecastElement = document.querySelector("#tableweek");
     forecastElement.innerHTML = null;
     let forecast = null;
 
@@ -108,3 +118,106 @@ let minutes = now.getMinutes();
 
 let dayHour = document.querySelector("#dayhour");
 dayHour.innerHTML = `${today} - ${hour}:${minutes}H`;
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+
+  function showWeatherfer(response) {
+    console.log(response);
+    let city = document.querySelector("#maincity");
+    let temperature = Math.round(response.data.main.temp);
+
+    let tempe = document.querySelector("#tempe");
+    let tempMax = document.querySelector("#temp-max");
+    let tempMin = document.querySelector("#temp-min");
+    let iconElement = document.querySelector("#main-icon");
+    let weathdescrip = document.querySelector("#weathdescrip");
+    let humidity = document.querySelector("#humidity");
+    let wind = document.querySelector("#wind");
+    let searchLat = `${response.data.coord.lat}`;
+    let searchLon = `${response.data.coord.lon}`;
+    city.innerHTML = `${response.data.name}`;
+    tempe.innerHTML = `${temperature}`;
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    weathdescrip.innerHTML = `${response.data.weather[0].description} |`;
+    humidity.innerHTML = `Humidity: ${response.data.main.humidity}% |`;
+    wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} mph`;
+    tempMax.innerHTML = `${Math.round(response.data.main.temp_max)}ºF`;
+    tempMin.innerHTML = `${Math.round(response.data.main.temp_min)}ºF`;
+
+    function showGeo(response) {
+      let forecastElement = document.querySelector("#tableweek");
+      forecastElement.innerHTML = null;
+      let forecast = null;
+
+      for (let i = 0; i < 6; i++) {
+        forecast = response.data.daily[i];
+
+        let nextday = i + 1;
+        let dayTeste = new Date(response.data.daily[nextday].dt * 1000);
+        let daysTeste = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ];
+        let realToday = daysTeste[dayTeste.getDay()];
+        let dailyMax = Math.round(response.data.daily[nextday].temp.max);
+        let dailyMin = Math.round(response.data.daily[nextday].temp.min);
+        let dailyIcon = response.data.daily[nextday].weather[0].icon;
+
+        forecastElement.innerHTML += `
+      <tbody>
+         <tr class="tablelines">
+           <td>${realToday}</td>
+           <td>${dailyMax}ºF/${dailyMin}ºF</td>
+           <td><img src = \http://openweathermap.org/img/wn/${dailyIcon}@2x.png\ width= 30px></td>
+           </tr>
+            </tbody>
+
+     `;
+      }
+    }
+
+    let geoApiKey = "013862d923ab37409ff3bafb37939a3d";
+    let geoUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${searchLat}&lon=${searchLon}&exclude=current,minutely,hourly&units=imperial&appid=${geoApiKey}`;
+
+    axios.get(geoUrl).then(showGeo);
+  }
+
+  function searchCity(city) {
+    let apiKey = "013862d923ab37409ff3bafb37939a3d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    axios.get(url).then(showWeatherfer);
+  }
+
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  searchCity(cityfer);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#tempe");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  searchCity(citycel);
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+}
+
+celsiusTemperature = null;
+cityfer = null;
+citycel = null;
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsius);
